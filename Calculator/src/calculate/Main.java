f786f88ce535bead249331552269b45106cac6a0
universe <universe@example.com>
2022-08-04 03:23:44 +0530

    PP-1

---
+++ Calculator/src/calculate/Main.java
@@ -0,0 +1,57 @@
+package calculate;
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2, result;
+
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+
+    // ask users to enter operator
+    System.out.println("Choose an operator:");
+    System.out.println("+\n-\n*\n/");
+    operator = input.next().charAt(0);
+
+    // ask users to enter numbers
+    System.out.println("Enter the first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter the second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+
+      // performs addition between numbers
+      case '+':
+        result = number1 + number2;
+        System.out.println("Answer:"+ " " +number1 + " + " + number2 + " = " + result);
+        break;
+
+      // performs subtraction between numbers
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator , enter from the operators provided above!");
+        break;
+    }
+
+    input.close();
+  }
+}
